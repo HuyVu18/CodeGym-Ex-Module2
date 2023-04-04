0bdec719c0d23def6a030c1433e06c77cebefc00
HuyVu18 <v.huy1808@gmail.com>
2023-04-04 16:53:30 +0700

    update file prime

---
+++ lesson2/Prime.java
@@ -0,0 +1,40 @@
+package lesson2;
+
+public class Prime {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        int num = input.nextInt();
+//        int count = 0;
+//
+//        for (int i = 2; i < 1000; i++) {
+//            Boolean check = true;
+//            if (num > count) {
+//                for (int j = 2; j <= Math.sqrt(i); j++) {
+//                    if (i % j == 0) {
+//                        check = false;
+//                    }
+//                }
+//                if (check) {
+//                    count += 1;
+//                    System.out.println(i);
+//                }
+//            }
+//        }
+//    }
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 100; i++) {
+            Boolean check = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    check = false;
+                }
+            }
+            if (check) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
