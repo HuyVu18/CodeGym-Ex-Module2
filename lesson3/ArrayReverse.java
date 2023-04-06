8910a0ec18a434eee65285ad1b0bcc6ff18f458b
HuyVu18 <v.huy1808@gmail.com>
2023-04-06 08:30:46 +0700

    update

--- lesson3/ArrayReverse.java
+++ lesson3/ArrayReverse.java
@@ -3,19 +3,43 @@ package lesson3;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class ArrayReverse {
     public static void main(String[] args) {
-        String[] arr = {"mm", "nn", "ll", "oo"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap kich thuoc mang");
+        int x = sc.nextInt();
+        int[] arr = new int[x];
+
+        System.out.println("Nhap tung phan tu");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
         System.out.println(Arrays.toString(arr));
+        System.out.println("Mang dao nguoc");
 
-        List list = Arrays.asList(arr);
-        Collections.reverse(list);
-
-        String[] arrs = (String[])list.toArray(new String[3]);
-        System.out.println(Arrays.toString(arrs));
-
+        for (int i = 0; i < arr.length/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[x - 1 - i];
+            arr[x - 1 - i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
     }
+
+
+
+
+//    public static void main(String[] args) {
+//        String[] arr = {"mm", "nn", "ll", "oo"};
+//        System.out.println(Arrays.toString(arr));
+//
+//        List list = Arrays.asList(arr);
+//        Collections.reverse(list);
+//
+//        String[] arrs = (String[])list.toArray(new String[3]);
+//        System.out.println(Arrays.toString(arrs));
+//    }
 }
 
 
--- lesson3/DelElementArr.java
+++ lesson3/DelElementArr.java
@@ -4,29 +4,52 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
+
 public class DelElementArr {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        arr.add(1);
-        arr.add(2);
-        arr.add(3);
-        arr.add(4);
-        arr.add(5);
-        System.out.println(arr);
-        System.out.println("nhap so can xoa");
-        int x = sc.nextInt();
-        int i = 0;
-        for (; i < arr.size(); i++) {
-            if (x == arr.get(i)) {
-                arr.remove(i);
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Nhap so muon xoa");
+        int a = sc.nextInt();
+        Boolean check = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == a) {
+                check = true;
+                for (; i < arr.length - 1; i++) {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.length - 1] = 0;
             }
         }
-        if (i == arr.size()) {
+        if (check) {
+            System.out.println("Mang da xoa so");
+            System.out.println(Arrays.toString(arr));
+        } else {
             System.out.println("So khong nam trong mang");
         }
-        System.out.println(arr);
 
 
+//        Scanner sc = new Scanner(System.in);
+//        ArrayList<Integer> arr = new ArrayList<Integer>();
+//        arr.add(1);
+//        arr.add(2);
+//        arr.add(3);
+//        arr.add(4);
+//        arr.add(5);
+//        System.out.println(arr);
+//        System.out.println("nhap so can xoa");
+//        int x = sc.nextInt();
+//        int i = 0;
+//        for (; i < arr.size(); i++) {
+//            if (x == arr.get(i)) {
+//                arr.remove(i);
+//            }
+//        }
+//        if (i == arr.size()) {
+//            System.out.println("So khong nam trong mang");
+//        }
+//        System.out.println(arr);
     }
 }
