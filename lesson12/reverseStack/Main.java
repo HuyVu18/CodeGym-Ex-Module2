1e1172b4d70913a836f914df8d032dc17c5d1a48
HuyVu18 <v.huy1808@gmail.com>
2023-04-17 08:32:10 +0700

    .

---
+++ lesson12/reverseStack/Main.java
@@ -0,0 +1,29 @@
+package lesson12.reverseStack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stk1 = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+        int input;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Nhap so thu " + (i + 1));
+            stk1.push(input = sc.nextInt());
+        }
+        System.out.println("Mang da nhap");
+        System.out.println(stk1);
+
+        Stack<Integer> stk2 = new Stack<>();
+        for (int i = 0; i < 5; i++) {
+            stk2.push(stk1.pop());
+        }
+        System.out.println("Mang dao nguoc");
+        System.out.println(stk2);
+    }
+
+
+
+
+}
