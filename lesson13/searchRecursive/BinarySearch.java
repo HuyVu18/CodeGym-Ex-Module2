81a795985ad3f9cce7fcd982ba2467d396c73b57
HuyVu18 <v.huy1808@gmail.com>
2023-04-18 14:48:46 +0700

    .

---
+++ lesson13/searchRecursive/BinarySearch.java
@@ -0,0 +1,19 @@
+package lesson13.searchRecursive;
+
+public class BinarySearch {
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+    private static final int notFound = -1;
+
+    static int search(int left, int right, int key) {
+        int mid = (left + right) / 2;
+        if (left <= right) {
+            if (key == list[mid])
+                return mid;
+            if (key < list[mid])
+                return search(left, mid - 1, key);
+            if (key > list[mid])
+                return search(mid + 1, right, key);
+        }
+        return notFound;
+    }
+}
---
+++ lesson13/searchRecursive/Main.java
@@ -0,0 +1,18 @@
+package lesson13.searchRecursive;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Search:");
+        int key = sc.nextInt();
+        int search1 = BinarySearch.search(0, BinarySearch.list.length, key);
+        if (search1 != -1) {
+            System.out.println("Element " + BinarySearch.list[search1] + " in Array and index " + search1);
+        } else {
+            System.out.println("Not Found!");
+        }
+    }
+}
